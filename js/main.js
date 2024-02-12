24566abe735978d552d3c86d8f3123cfc9c323f5
Michael Hammond <mickeymond@gmail.com>
2024-02-12 13:54:25 +0000

    Working on Appy Navbar

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// alert('We are Beta!!!');
+// console.log('We are Beta!!!');
+var firstName = 'Michael';
+var age = 9;
+
+let lastName = "Hammond";
+const boilingWaterTemperature = 100;
+
+var firstName = 'Eunice';
+// let lastName = 'Asare';
+// const boilingWaterTemperature = 120;
+
+let wannaDie = true;
+
+let bottle = {
+    color: 'blue',
+    volume: 580,
+    canStoreWater: true,
+    storeWater: null
+}
+
+// console.log(firstName, age, lastName, boilingWaterTemperature);
+
+// Conditional Statements (If/Else)
+if (age >= 18) {
+    console.log('You are eligible.');
+} else {
+    console.log('Wait till you are eigtheen.');
+}
+
+// Loops (For)
+for (let i = 1; i == 0; i++) {
+    console.log('I am at index:', i);
+}
